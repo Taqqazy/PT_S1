76c4a6a6bce1e7f2b5f53153f292540728c9a199
louislecc <louisdorsay@gmail.com>
2019-12-18 10:50:18 +0100

    18/12

---
+++ pullsh.cpp
@@ -0,0 +1,16 @@
+#include "pullsh.h"
+
+vector<string> lineToVec(const string &ligne) {
+    vector<string> vectemp;
+    int i=0;
+    for(int y=0;y<6;y++) {
+        string strtemp="";
+        while(ligne[i]!='|' && i<ligne.size()) {
+            strtemp+=ligne[i];
+            i++;
+        }
+        vectemp.push_back(strtemp);
+    }
+
+    return vectemp;
+}
---
+++ pullsh.h
@@ -0,0 +1,13 @@
+#ifndef PULLSH_H_INCLUDED
+#define PULLSH_H_INCLUDED
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<string> lineToVec(const string &ligne);
+
+
+
+#endif // PULLSH_H_INCLUDED
