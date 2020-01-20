6fcdcf6eb054e26c0b48876a86cd19a3f49616f9
louislecc <louisdorsay@gmail.com>
2020-01-20 22:01:01 +0100

    style

--- makeCss.cpp
+++ makeCss.cpp
@@ -4,8 +4,7 @@ void makeCss(ofstream &css, bool mode) {
     if(mode) {
         css << ".logo_ent {" << endl;
         css << "    display: block;" << endl;
-        css << "    margin-left: auto;" << endl;
-        css << "    margin-right: auto;" << endl;
+        css << "    margin: 0 auto 20px;" << endl;
         css << "}" << endl;
     }
     css << ".photo {" << endl;
@@ -16,6 +15,7 @@ void makeCss(ofstream &css, bool mode) {
     css << "    border: solid red;" << endl;
     css << "    width: 105px;" << endl;
     css << "    height: 135px;" << endl;
+    css << "    margin: 0 auto;" << endl;
     css << "}" << endl;
     css << ".no_photo p {" << endl;
     css << "    margin-top: 0;" << endl;
@@ -23,5 +23,22 @@ void makeCss(ofstream &css, bool mode) {
     css << "    text-align: center;" << endl;
     css << "    vertical-align: middle;" << endl;
     css << "    line-height: 135px;" << endl;
+    css << "}" << endl;
+    css << ".ent {" << endl;
+    css << "    border: solid black;" << endl;
+    css << "    padding: 20px;" << endl;
+    css << "    width: 90%;" << endl;
+    css << "    height: auto;" << endl;
+    css << "    text-align: center;" << endl;
+    css << "    margin: 0 auto 30px auto;" << endl;
+    css << "}" << endl;
+    css << ".pers {" << endl;
+    css << "    width: auto;" << endl;
+    css << "    float: left;" << endl;
+    css << "    height: 200px;" << endl;
+    css << "    background-color: #FFF;" << endl;
+    css << "    border: 1px solid #000;" << endl;
+    css << "    margin: 10px;" << endl;
+    css << "    text-align: center;" << endl;
     css << "}" << endl;
 }
